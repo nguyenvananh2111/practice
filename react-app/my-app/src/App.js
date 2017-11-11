cebfb85ccb206a79455e27ed32fbe70c18ee236e
nguyenvananh2111 <nguyenvananh2111@gmail.com>
2017-11-11 21:27:37 +0700

    Todo list

---
+++ react-app/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Todo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Todo />
+      </div>
+    );
+  }
+}
+
